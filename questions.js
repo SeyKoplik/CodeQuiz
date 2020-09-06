8ffdd46d4ef9f1b666131926d9c3a9c88ecbd16e
Seychelle Koplik <sey.koplik@gmail.com>
2020-09-06 14:56:43 -0400

    questions object added

---
+++ questions.js
@@ -0,0 +1,42 @@
+var questionSelection = [
+    {
+    question: "Which of the following is not considered a web development language?",
+    choiceA : "HTML",
+    choiceB : "CSS",
+    choiceC : "ADD",
+    optionD : "JavaScript",
+    correct : "ADD"
+    },  
+    {
+        question: "What is the correct syntax to create a function",
+        choiceA : "createFunction:new;",
+        choiceB : "function newFunc()",
+        choiceC : "(newFunc)function =",
+        optionD : "Function=newFunc;",
+        correct : "function newFunc()"
+        },  
+        {
+            question: "How would you create a pop up box with a text input box?",
+            choiceA : "alert",
+            choiceB : "prompt",
+            choiceC : "ask",
+            optionD : "confirm",
+            correct : "prompt"
+            },  
+            {
+                question: "Which of the following is not a valid example of a self closing tag",
+                choiceA : "<br>",
+                choiceB : "<img>",
+                choiceC : "<script>",
+                optionD : "<hr>",
+                correct : "<script>"
+                },  
+                {
+                    question: "The box model of CSS has the following properties of: border, margin, padding, and ______ ?",
+                    choiceA : "context",
+                    choiceB : "content",
+                    choiceC : "container",
+                    optionD : "collagen",
+                    correct : "content"
+                    },  
+];
