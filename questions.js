fcb91b6bff4cc803a99acafc70eed2852a10281c
Seychelle Koplik <sey.koplik@gmail.com>
2020-09-07 11:40:58 -0500

    modified questions index

--- questions.js
+++ questions.js
@@ -4,39 +4,39 @@ var questionSelection = [
     choiceA : "HTML",
     choiceB : "CSS",
     choiceC : "ADD",
-    optionD : "JavaScript",
-    correct : "ADD"
+    choiceD : "JavaScript",
+    answer : "ADD"
     },  
     {
         question: "What is the correct syntax to create a function",
         choiceA : "createFunction:new;",
         choiceB : "function newFunc()",
         choiceC : "(newFunc)function =",
-        optionD : "Function=newFunc;",
-        correct : "function newFunc()"
+        choiceD : "Function=newFunc;",
+        answer : "function newFunc()"
         },  
         {
             question: "How would you create a pop up box with a text input box?",
             choiceA : "alert",
             choiceB : "prompt",
             choiceC : "ask",
-            optionD : "confirm",
-            correct : "prompt"
+            choiceD : "confirm",
+            answer : "prompt"
             },  
             {
                 question: "Which of the following is not a valid example of a self closing tag",
                 choiceA : "<br>",
                 choiceB : "<img>",
                 choiceC : "<script>",
-                optionD : "<hr>",
-                correct : "<script>"
+                choiceD : "<hr>",
+                answer : "<script>"
                 },  
                 {
                     question: "The box model of CSS has the following properties of: border, margin, padding, and ______ ?",
                     choiceA : "context",
                     choiceB : "content",
                     choiceC : "container",
-                    optionD : "collagen",
-                    correct : "content"
+                    choiceD : "collagen",
+                    answer : "content"
                     },  
 ];
--- testScript.js
+++ testScript.js
@@ -1,14 +1,68 @@
 
 var openingPage = document.getElementById("openingPage");
 var questionsPage = document.getElementById("questionsPage");
+var timer = document.getElementById("timer");
+var timer2 = document.getElementById("timer2");
+var gameOverPage = document.getElementById("gameOverPage");
+var timeLeft = 60;
+var timesDone = 0;
 
+document.getElementById("beginQuiz").addEventListener('click', startQuiz);
 
-document.getElementById("beginQuiz").addEventListener('click',startQuiz);
 
 function startQuiz() {
     openingPage.style.display = "none";
     questionsPage.style.display = "block";
+    timer.style.display = "block";
+    timeStart();
+    showQuestions();
+  
 }
 
+function timeStart() {
+    var timeLeft = 60;
+  
+    var timeInterval = setInterval(function() {
+      timer2.textContent = timeLeft;
+      timeLeft--;
 
+  
+      if (timeLeft == -1) {
+        timer2.textContent = "0";
+        questionsPage.style.display = "none";
+        gameOverPage.style.display = "block";
+        clearInterval(timeInterval);
 
+      }
+  
+    }, 1000);
+  }
+
+var questionsAsked = document.getElementById("questionAsked");
+var answerA = document.getElementById("answerA");
+var answerB = document.getElementById("answerB");
+var answerC = document.getElementById("answerC");
+var answerD = document.getElementById("answerD");
+
+questionSelection[0].question
+questionSelection[0].choiceA
+questionSelection[0].choiceB
+questionSelection[0].choiceC
+questionSelection[0].choiceD
+questionSelection[0].answer
+
+var lastQuestionIndex = questionSelection.length -1;
+var beginQuestionIndex = 0;
+
+function showQuestions(){
+    var x = questionSelection[beginQuestionIndex];
+    questionsAsked.innerHTML = x.questionSelection;
+    answerA.innerHTML = x.choiceA;
+    answerB.innerHTML = x.choiceB;
+    answerC.innerHTML = x.choiceC;
+    answerD.innerHTML = x.choiceD;
+}
+
+// showQuestions()
+// beginQuestionIndex++
+// showQuestions()
